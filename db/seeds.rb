a6d66832bf2649d56d685a10133eed4f4611eb60
aurelien bigo <aurelien.bigo@gmail.com>
2017-08-08 15:54:23 +0200

    lister créer article

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+10.times do
+  Article.create!(
+    title: Faker::Simpsons.character,
+    content: Faker::Simpsons.quote )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
